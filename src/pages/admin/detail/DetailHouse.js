aca06fed883f1856f3399632a003e0c9c281fc93
viemonkey <haducbaoviet@gmail.com>
2023-09-19 14:18:26 +0700

    before merce

--- src/pages/admin/detail/DetailHouse.js
+++ src/pages/admin/detail/DetailHouse.js
@@ -1,7 +1,35 @@
 import Navbar from "../../../components/navbar/Navbar";
 import StarIcon from '@mui/icons-material/Star';
+import Footer from "../../../components/footer/Footer";
+import { Box, Modal } from "@mui/material";
+import React, { useState } from 'react';
+import { DatePicker, Space } from 'antd';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+};
 
 export default function DetailHouse() {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const onChange = (date, dateString) => {
+        console.log(date, dateString);
+    };
+
+    const App = () => (
+        <Space direction="vertical">
+            <DatePicker onChange={onChange} />
+        </Space>
+    );
     return (
         <div className="row">
             <div className="col-12">
@@ -67,14 +95,74 @@ export default function DetailHouse() {
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="sr-only">Next</span>
             </button>
-            {/* <div className="col-12">
+            <div className="col-12 mt-4" style={{ padding: "0 80px", display: "flex" }}>
                 <div className="col-8">
+                    <div style={{ display: "flex" }}>
+                        <h3>Toàn bộ nhà gỗ chalet. Chủ nhà Tanja</h3>
+                    </div>
+                    <div style={{ display: "flex" }}>
+                        <div>
+                            7 khach .
+                        </div>
+                        <div>
+                            4 phong ngu .
+                        </div>
+                        <div>
+                            7 giuong .
+                        </div>
+                        <div>
+                            1 phòng tắm đầy đủ và 1 phòng vệ sinh cơ bản riêng
+                        </div>
+                    </div>
+                    <div style={{ border: "0.5px solid gray", width: "100%", margin: "20px 0" }}></div>
+                    <div style={{ textAlign: "left" }}>
+                        Một số thông tin đã được dịch tự động. Hiển thị ngôn ngữ gốc
+                        Đây là nhà gỗ 4 phòng ngủ chất lượng cao và được trang bị đầy đủ tiện nghi tại khu nghỉ dưỡng trượt tuyết Lapland xinh đẹp và khu nghỉ dưỡng trượt tuyết lớn nhất Phần Lan Levi.
 
+                        200m đến dốc, trạm xe buýt trượt tuyết bên cạnh và làng Levi cách đó 10 phút.
+
+                        Nhà gỗ chalet có bếp/phòng chờ mở rộng rãi, với cửa sổ lớn để tận hưởng tầm nhìn đẹp.
+                    </div>
                 </div>
                 <div className="col-4">
-
+                    <div style={{ width: "390", height: "480px", border: "0.5px solid gray", borderRadius: "10px", padding: "24px", boxShadow: "1px 2px 9px #DADADA" }}>
+                        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "flex-start" }}>
+                            <div>
+                                $101 / đêm
+                            </div>
+                            <div style={{ display: "flex" }}>
+                                <div>
+                                    <StarIcon /> 4,90 .
+                                </div>
+                                <div>
+                                    40 đánh giá
+                                </div>
+                            </div>
+                        </div>
+                        <div onClick={handleOpen} style={{ border: "0.5px solid", display: "flex", borderRadius: "10px", textAlign: "center", marginTop: "20px", cursor: "pointer" }}>
+                            <div style={{ borderRight: "0.5px solid", width: "150px", padding: "20px" }}>
+                                Nhận phòng
+                            </div>
+                            <div style={{ padding: "20px", textAlign: "center" }}>
+                                Trả phòng
+                            </div>
+                            <Modal
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                        >
+                            <Box sx={style}>
+                                <App />
+                            </Box>
+                        </Modal>
+                        </div>
+                    </div>
                 </div>
-            </div> */}
+            </div>
+            <div className="col-12" style={{ padding: "0", marginTop: "20px" }}>
+                <Footer />
+            </div>
         </div>
     )
 }
