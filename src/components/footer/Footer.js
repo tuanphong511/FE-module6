2c171835e4904ac6a64a77429e3a05d6261bf230
tuanphong511 <tuanphong511@gmail.com>
2023-09-20 10:11:08 +0700

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -24,54 +24,52 @@ export default function Footer() {
                                         nhiều người tin tưởng
                                     </p>
                                 </div>
-                                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                                    <h6 class="text-uppercase fw-bold">Products</h6>
-                                    <hr
-                                        class="mb-4 mt-0 d-inline-block mx-auto"
-                                        style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }}
-                                    />
-                                    <p>
-                                        <a href="#!" class="text-dark">MDBootstrap</a>
-                                    </p>
-                                    <p>
-                                        <a href="#!" class="text-dark">MDWordPress</a>
-                                    </p>
-                                    <p>
-                                        <a href="#!" class="text-dark">BrandFlow</a>
-                                    </p>
-                                    <p>
-                                        <a href="#!" class="text-dark">Bootstrap Angular</a>
-                                    </p>
-                                </div>
+                                {/*<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">*/}
+                                {/*    <h6 class="text-uppercase fw-bold">Products</h6>*/}
+                                {/*    <hr*/}
+                                {/*        class="mb-4 mt-0 d-inline-block mx-auto"*/}
+                                {/*        style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }}*/}
+                                {/*    />*/}
+                                {/*    <p>*/}
+                                {/*        <a href="#!" class="text-dark">MDBootstrap</a>*/}
+                                {/*    </p>*/}
+                                {/*    <p>*/}
+                                {/*        <a href="#!" class="text-dark">MDWordPress</a>*/}
+                                {/*    </p>*/}
+                                {/*    <p>*/}
+                                {/*        <a href="#!" class="text-dark">BrandFlow</a>*/}
+                                {/*    </p>*/}
+                                {/*    <p>*/}
+                                {/*        <a href="#!" class="text-dark">Bootstrap Angular</a>*/}
+                                {/*    </p>*/}
+                                {/*</div>*/}
                                 <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                                    <h6 class="text-uppercase fw-bold">Useful links</h6>
+                                    <h6 class="text-uppercase fw-bold">Tiện ích</h6>
                                     <hr
                                         class="mb-4 mt-0 d-inline-block mx-auto"
                                         style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }}
                                     />
+
                                     <p>
-                                        <a href="#!" class="text-dark">Your Account</a>
+                                        <a href="#!" class="text-dark">Giới thiệu</a>
                                     </p>
                                     <p>
-                                        <a href="#!" class="text-dark">Become an Affiliate</a>
+                                        <a href="#!" class="text-dark">Chủ công ty</a>
                                     </p>
                                     <p>
-                                        <a href="#!" class="text-dark">Shipping Rates</a>
-                                    </p>
-                                    <p>
-                                        <a href="#!" class="text-dark">Help</a>
+                                        <a href="#!" class="text-dark">Hỗ trợ</a>
                                     </p>
                                 </div>
                                 <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                                    <h6 class="text-uppercase fw-bold">Contact</h6>
+                                    <h6 class="text-uppercase fw-bold">Liên lạc</h6>
                                     <hr
                                         class="mb-4 mt-0 d-inline-block mx-auto"
                                         style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }}
                                     />
-                                    <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                                    <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-                                    <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                                    <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                                    <p><i class="fas fa-home mr-3"></i> CodeGym , Mỹ Đình , Hà Nội</p>
+                                    <p><i class="fas fa-envelope mr-3"></i>haducbaoviet@gmail.com </p>
+                                    <p><i class="fas fa-phone mr-3"></i> 0869 854 360</p>
+
                                 </div>
                             </div>
                         </div>
--- src/components/house/listHouse.js
+++ src/components/house/listHouse.js
@@ -96,64 +96,7 @@ export default function ListHouse() {
                     )
                 }else {
                     return (
-                        <div className="col-3 p-5" style={{cursor: "pointer", marginTop: "10px"}} key={key}>
-                            <div className="card" style={{width: "20rem", borderRadius: "10px"}}>
-                                <div id={`home${key}`} className="carousel slide" data-ride="carousel">
-                                    <ol className="carousel-indicators">
-                                        {item.picture.map((picture, index) => (
-                                            <li
-                                                key={index}
-                                                data-target={`#home${key}`}
-                                                data-slide-to={index}
-                                                className={index === 0 ? "active" : ""}
-                                            ></li>
-                                        ))}
-                                    </ol>
-                                    <div className="carousel-inner" onClick={(e) => {
-                                        handleDetail(e)
-                                    }}>
-                                        {item.picture.map((picture, index) => (
-                                            <div
-                                                key={index}
-                                                className={`carousel-item ${index === 0 ? "active" : ""}`}
-                                            >
-                                                <img className="d-block w-100" src={picture.picture}
-                                                     style={{borderRadius: "10px", height: "270px", width: "280px"}}
-                                                     alt={`Slide ${index + 1}`}/>
-                                            </div>
-                                        ))}
-                                    </div>
-                                    <a
-                                        className="carousel-control-prev"
-                                        href={`#home${key}`}
-                                        role="button"
-                                        data-slide="prev"
-                                    >
-                                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                        <span className="sr-only">Previous</span>
-                                    </a>
-                                    <a
-                                        className="carousel-control-next"
-                                        href={`#home${key}`}
-                                        role="button"
-                                        data-slide="next"
-                                    >
-                                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                        <span className="sr-only">Next</span>
-                                    </a>
-                                </div>
-                                <div className="card-body" style={{textAlign: "left"}} onClick={(e) => {
-                                    handleDetail(e)
-                                }}>
-                                    <h5 className="card-title">{item.name}</h5>
-                                    <h5 className="card-title">{item.address}</h5>
-                                    <h5 className="card-title">{item.price} / đêm</h5>
-                                    <p className="card-text">{item.description}</p>
-                                </div>
-
-
-                            </div>
-                        </div>
+                       <></>
                     )
                 }
 
